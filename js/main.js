063c68e7d1a59dde9981ead36565b5930023e1f8
Caio Sampaio <caio6c@yahoo.com.br>
2020-08-30 13:12:20 -0300

    Alteração no valor do As dependendo do total do resultado.

--- js/main.js
+++ js/main.js
@@ -331,7 +331,11 @@ function hit() {
             c10 = 10
             c11 = 10
             c12 = 10
-            c13 = 11
+            if (resultado + 11 > 21) {
+                c13 = 1
+            } else {
+                c13 = 11
+            }
             c14 = 2
             c15 = 3
             c16 = 4
@@ -344,7 +348,11 @@ function hit() {
             c23 = 10
             c24 = 10
             c25 = 10
-            c26 = 11
+            if (resultado + 11 > 21) {
+                c26 = 1
+            } else {
+                c26 = 11
+            }
             c27 = 2
             c28 = 3
             c29 = 4
@@ -357,7 +365,11 @@ function hit() {
             c36 = 10
             c37 = 10
             c38 = 10
-            c39 = 11
+            if (resultado + 11 > 21) {
+                c39 = 1
+            } else {
+                c39 = 11
+            }
             c40 = 2
             c41 = 3
             c42 = 4
@@ -370,7 +382,11 @@ function hit() {
             c49 = 10
             c50 = 10
             c51 = 10
-            c52 = 11
+            if (resultado + 11 > 21) {
+                c52 = 1
+            } else {
+                c52 = 11
+            }
 
             vetorCartaMais1 = ["erro", c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19, c20, c21, c22, c23, c24, c25, c26, c27, c28, c29, c30, c31, c32, c33, c34, c35, c36, c37, c38, c39, c40, c41, c42, c43, c44, c45, c46, c47, c48, c49, c50, c51, c52];
             vetorCartaMais2 = ["erro", c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19, c20, c21, c22, c23, c24, c25, c26, c27, c28, c29, c30, c31, c32, c33, c34, c35, c36, c37, c38, c39, c40, c41, c42, c43, c44, c45, c46, c47, c48, c49, c50, c51, c52];
